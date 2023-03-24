e72ce9d17774155be700c719c9cfdfd276aec77d
FREDVUNI <fredvuni809@gmail.com>
2023-03-24 15:29:29 +0300

    create user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    @Get()
+    getUsers(){
+        return { name:"vuni",email:"fredvuni809@gmail.com" }    
+    }
+
+    @Post()
+    createUser(@Req() req:Request){
+        return req.body
+    }
+
+    @Get('/:userId')
+    getUser(@Param() params: {userId: Number}){
+        return params
+    }
+
+    @Patch('/:userId')
+    updateUser(@Req() req:Request){
+        return req.body
+    }
+
+    @Delete('/:userId')
+    deleteUser(@Param() params:{userId:Number}){
+        return params
+    }
+
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({})
-export class UserModule {}
+export class UserModule {
+    controllers:[UserController];
+    services:[UserService];
+}
