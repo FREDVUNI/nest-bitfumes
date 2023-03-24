59381f1a401c02e60ea5f91cd25d8b1b86853a0a
FREDVUNI <fredvuni809@gmail.com>
2023-03-24 20:28:15 +0300

    add service to category

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,7 +1,27 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable,Param,Req } from '@nestjs/common'
+import { Request } from 'express'
 
 @Injectable()
 
 export class CategoryService{
 
+    categories(){
+        return { category:"Electronics" }
+    }
+
+    show(@Param() param:{ categoryId: number }){
+        return param
+    }
+
+    store(@Req() req:Request){
+        return req.body
+    }
+
+    update(@Req() req:Request, @Param() param:{categoryId:number}){
+        return { body:req.body, param: param}
+    }
+
+    delete(@Param() param:{categoryId:number}){
+        return param
+    }
 }
