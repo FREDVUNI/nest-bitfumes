4bc581cf0a822c329c22987294383ebe6a5333e2
FREDVUNI <fredvuni809@gmail.com>
2023-03-24 20:18:53 +0300

    create module category

---
+++ src/category/category.controller.ts
@@ -0,0 +1,30 @@
+import { Controller,Get,Patch,Post,Delete } from '@nestjs/common'
+
+@Controller('category')
+export class CategoryController{
+
+    @Get()
+    getCategories(){
+        
+    }
+
+    @Get()
+    getCategory(){
+        
+    }
+
+    @Post()
+    createCategory(){
+        
+    }
+
+    @Patch()
+    updateCategory(){
+
+    }
+
+    @Delete()
+    deleteCategory(){
+
+    }
+}
---
+++ src/category/category.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { CategoryController } from './category.controller'
+import { CategoryService } from './category.service'
+
+@Module({
+    controllers:[CategoryController],
+    providers:[CategoryService]
+})
+
+export class CategoryModule{
+
+}
