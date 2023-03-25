84e2404ced2649b806fa274e615cf19290e511de
FREDVUNI <fredvuni809@gmail.com>
2023-03-25 16:38:48 +0300

    register product

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,13 @@ import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { CategoryController } from './category/category.controller';
 import { CategoryService } from './category/category.service';
+import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController, UserController,CategoryController],
-  providers: [AppService, UserService,CategoryService],
+  imports: [UserModule,ProductModule],
+  controllers: [AppController, UserController,CategoryController,ProductController],
+  providers: [AppService, UserService,CategoryService,ProductService],
 })
 export class AppModule {}
