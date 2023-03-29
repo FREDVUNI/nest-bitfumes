d5afb05bbabd83145f4e2bdfa49c4e4bcdb473d1
FREDVUNI <fredvuni809@gmail.com>
2023-03-29 12:25:07 +0300

    add valiadtions to product dto

--- src/product/dto/create.product.dto.ts
+++ src/product/dto/create.product.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class createProductDto{
+    @IsString()
+    @IsNotEmpty()
     product:string;
+
+    @IsString()
+    @IsNotEmpty()
     price:string;
+
+    @IsString()
+    @IsNotEmpty()
     details:string;
 }
--- src/product/dto/update.product.dto.ts
+++ src/product/dto/update.product.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString } from "class-validator/types/decorator/decorators";
+
 export class updateProductDto{
+    @IsString()
+    @IsNotEmpty()
     product:string;
+
+    @IsString()
+    @IsNotEmpty()
     price:string;
+
+    @IsString()
+    @IsNotEmpty()
     details:string;
 }
