5906e68d6c6a12fd174721d99982b64336b4ab27
FREDVUNI <fredvuni809@gmail.com>
2023-03-29 12:23:39 +0300

    add valiadtions to user dto

--- src/user/dto/create.user.dto.ts
+++ src/user/dto/create.user.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 export class createUserDto{
+    @IsString()
+    @IsNotEmpty()
     username:string;
+
+    @IsEmail()
+    @IsNotEmpty()
     email:string;
+
+    @IsString()
+    @IsNotEmpty()
     password:string;
 }
--- src/user/dto/update.user.dto.ts
+++ src/user/dto/update.user.dto.ts
@@ -1,5 +1,15 @@
+import { IsString,IsEmail, IsNotEmpty } from "class-validator";
+
 export class updateUserDto{
+    @IsString()
+    @IsNotEmpty()
     username:string;
+
+    @IsEmail()
+    @IsNotEmpty()
     email:string;
+
+    @IsString()
+    @IsNotEmpty()
     password:string;
 }
