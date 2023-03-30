fabc24ee9bda7d62e73f837ad4c56bf691edcd15
FREDVUNI <fredvuni809@gmail.com>
2023-03-30 10:39:56 +0300

    add user entity and create db connection

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { User } from './entity/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
     controllers:[UserController],
-    providers:[UserService]
+    providers:[UserService],
+    imports:[TypeOrmModule.forFeature([User])]
 })
 
 export class UserModule {
