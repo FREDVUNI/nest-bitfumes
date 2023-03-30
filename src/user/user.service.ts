4849bac8a196d0d0b85191fe670ad2c291d1d80a
FREDVUNI <fredvuni809@gmail.com>
2023-03-30 11:08:44 +0300

    set up user entity in service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { Request } from 'express';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { createUserDto } from './dto/create.user.dto';
 import { updateUserDto } from './dto/update.user.dto';
+import { User } from './entity/user.entity';
 
 @Injectable()
 export class UserService {
-
+    constructor(
+        @InjectRepository(User)
+        private userRepository:Repository<User>
+    ){} 
     get(){
         return { name:"vuni",email:"fredvuni809@gmail.com" }
     }
