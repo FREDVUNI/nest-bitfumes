5116896bbc82e6f056ae75953bf872bcffd36bf2
FREDVUNI <fredvuni809@gmail.com>
2023-03-30 10:11:05 +0300

    add not empty on category dto

--- src/category/dto/create.category.dto.ts
+++ src/category/dto/create.category.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from "class-validator/types/decorator/decorators";
+import { IsString,IsNotEmpty } from "class-validator/types/decorator/decorators";
 
 export class createCategoryDto{
+    @IsNotEmpty()
     @IsString()
     category: string;
 }
--- src/category/dto/update.category.dto.ts
+++ src/category/dto/update.category.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from "class-validator/types/decorator/decorators";
+import { IsString,IsNotEmpty } from "class-validator/types/decorator/decorators";
 
 export class updateCategoryDto{
+    @IsNotEmpty()
     @IsString()
     category: string;
 }
