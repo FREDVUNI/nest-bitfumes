fe131c9fbafdfd9ae999e61f483826b43976f90c
FREDVUNI <fredvuni809@gmail.com>
2023-04-01 13:14:43 +0300

    set up controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Post,Body } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
+
 export class AuthController {
-    
+    constructor(){}
+    @Post('/login')
+    async login(@Body() AuthDto:any){
+        return
+    }
 }
